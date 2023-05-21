24677757d49b57c0e77724a8a15383fb6243ec7d
RafaelTuber <RafaelTuber@hotmail.com>
2023-05-21 15:49:08 -0300

    criado endpoint de login

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,16 +1,19 @@
-import type {NextApiRequest, NextApiResponse} from 'next';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
+import type { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg';
 
-// eslint-disable-next-line import/no-anonymous-default-export
-export default (
+const endpointLogin = (
     req: NextApiRequest,
-    res: NextApiResponse
+    res: NextApiResponse<RespostaPadraoMsg>
 ) => {
-    if(req.method === 'POST'){
-        const {login, password} = req.body;
-        if(login === 'admin@admin.com' && password === 'Admin@123'){
-            res.status(200).json({msg: 'Usuário autenticado com sucesso'});
+    if (req.method === 'POST') {
+        const { login, password } = req.body;
+        if (login === 'admin@admin.com' && password === 'Admin@123') {
+            return res.status(200).json({ msg: 'Usuário autenticado com sucesso' });
         }
-        return res.status(400).json({ erro: 'Usuário ou senha não encontrado'});
+        return res.status(400).json({ erro: 'Usuário ou senha não encontrado' });
     }
-    return res.status(405).json({erro: 'Método informado não é valido'});
+    return res.status(405).json({ erro: 'Método informado não é valido' });
 }
+
+export default conectarMongoDB(endpointLogin);
