44f0ceec27bd2bda81971a3d16d871fbaefd474b
RafaelTuber <RafaelTuber@hotmail.com>
2023-05-22 00:07:08 -0300

    atualizado pagina de cadastro com verificação e conexão no banco de dados

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -2,6 +2,8 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import type { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg';
 import type { CadastroRequisicao } from '../../types/CadastroRequisicao';
 import { UsuarioModel } from '../../models/UsuarioModel';
+import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
+import md5 from 'md5';
 
 const endpointCadastro =
     async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
@@ -22,11 +24,21 @@ const endpointCadastro =
             if(!usuario.senha || usuario.senha.length < 4){
                 return res.status(400).json({erro: 'Senha invalido!'});
             }
+            // validação se já existe usuário com o mesmo email
+            const usuariosComMesmoEmail = await UsuarioModel.find({email: usuario.email});
+            if(usuariosComMesmoEmail && usuariosComMesmoEmail.length > 0){
+                return res.status(400).json({erro: 'Já existe uma conta com o email informado!'});
+            }
             // salvar dados no banco de dados
-            await UsuarioModel.create(usuario);
+            const usuarioASerSalvo = {
+                nome : usuario.nome,
+                email : usuario.email,
+                senha : md5(usuario.senha),
+            }
+            await UsuarioModel.create(usuarioASerSalvo);
             return res.status(200).json({msg: 'Usuário cadastrado com sucesso'});
         }
         return res.status(405).json({ erro: 'Método informado não é valido' });
     }
 
-export default endpointCadastro;
+export default conectarMongoDB(endpointCadastro);
