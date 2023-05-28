9b0237688949b278892a624e95730ffbe9dc2b8d
RafaelTuber <RafaelTuber@hotmail.com>
2023-05-28 16:01:06 -0300

    pesquisa de usuario

---
+++ pages/api/pesquisa.ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import type { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg';
+import { validarTokenJWT } from '../../middlewares/validarTokenJWT';
+import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
+import { UsuarioModel } from "../../models/UsuarioModel";
+import nc from 'next-connect';
+
+const pesquisaEndPoint
+    = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg | any[]>) => {
+        try {
+            if(req.method === 'GET'){
+                const {filtro} = req.query;
+                //buscar nome pelo filtro
+                // ou buscar email
+                if(!filtro || filtro.length < 2){                    
+                    return res.status(400).json({ erro: 'Favor informar 2 caracteres para a busca '});
+                }
+                const usuarioEncontrados = await UsuarioModel.find({
+                    $or: [{ nome : {$regex : filtro, $options : 'i'}},
+                    { email : {$regex : filtro, $options : 'i'}}]
+                    
+                });
+
+                return res.status(200).json(usuarioEncontrados);
+            }
+            return res.status(405).json({ erro: 'Metodo informado nao e valido '});
+        } catch (e) {
+            console.log(e);
+            return res.status(500).json({ erro: 'Não foi possivel buscar usuarios: ' + e });
+        }
+    }
+
+export default validarTokenJWT(conectarMongoDB(pesquisaEndPoint));
