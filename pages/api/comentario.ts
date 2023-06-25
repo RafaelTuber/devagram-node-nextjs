c3cf4f80a3b85725d1abc5eb6ff4cf5019430450
RafaelTuber <RafaelTuber@hotmail.com>
2023-06-25 03:49:07 -0300

    criado endpoint de adicionar comentarios

---
+++ pages/api/comentario.ts
@@ -0,0 +1,46 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import type { RespostaPadraoMsg } from "../../types/RespostaPadraoMsg";
+import { validarTokenJWT } from '../../middlewares/validarTokenJWT';
+import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
+import { UsuarioModel } from '../../models/UsuarioModel';
+import { PublicacaoModel } from '../../models/PublicacaoModel';
+
+const comentarioEndpoint = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
+    try {
+        // metodo http que estaremos utilizando
+        if(req.method === 'PUT'){
+            // id usuario
+            const {userId, id} = req.query;
+            const usuarioLogado = await UsuarioModel.findById(userId);
+            if(!usuarioLogado){
+                return res.status(400).json({erro : 'Usuario nao encontrado'});
+            }
+            // id publicacao
+            const publicacao =  await PublicacaoModel.findById(id);
+            if(!publicacao){
+                return res.status(400).json({erro : 'Publicacao nao encontrada'});
+            }
+            // id comentario
+            if(!req.body || !req.body.comentario
+                || req.body.comentario.length < 2){
+                return res.status(400).json({erro : 'Comentario nao e valido'});
+            }
+
+            const comentario = {
+                usuarioId : usuarioLogado._id,
+                nome : usuarioLogado.nome,
+                comentario : req.body.comentario
+            }
+
+            publicacao.comentarios.push(comentario);
+            await PublicacaoModel.findByIdAndUpdate({_id : publicacao._id}, publicacao);
+            return res.status(200).json({msg : 'Comentario adicionado com sucesso'});
+        }
+        return res.status(405).json({ erro: 'Metodo informado nao é valido' });
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({erro : 'Ocorreu erro ao adicionar comentario'});
+    }
+}
+
+export default validarTokenJWT(conectarMongoDB(comentarioEndpoint));
